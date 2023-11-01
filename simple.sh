6e3d648352e8ae129461c3470aa1bf37dc90be82
vagrant <vagrant@vagrant.vm>
2023-11-01 16:18:55 +0000

    All the files are sample file

---
+++ simple.sh
@@ -0,0 +1,105 @@
+It was 7 minutes after midnight. The dog was lying on the grass in the middle of the lawn in front of Mrs Shears’ house. Its eyes were closed. It looked as if it was running on its side, the way dogs run when they think they are chasing a cat in a dream. But the dog was not running or asleep. The dog was dead. There was a garden fork sticking out of the dog. The points of the fork must have gone all the way through the dog and into the ground because the fork had not fallen over. I decided that the dog was probably killed with the fork because I could not see any other wounds in the dog and I do not think you would stick a garden fork into a dog after it had died for some other reason, like cancer for example, or a road accident. But I could not be certain about this.
+
+I went through Mrs Shears’ gate, closing it behind me. I walked onto her lawn and knelt beside the dog. I put my hand on the muzzle of the dog. It was still warm.
+
+The dog was called Wellington. It belonged to Mrs Shears who was our friend. She lived on the opposite side of the road, two houses to the left.
+
+Wellington was a poodle. Not one of the small poodles that have hairstyles but a big poodle. It had curly black fur, but when you got close you could see that the skin underneath the fur was a very pale yellow, like chicken.
+
+I stroked Wellington and wondered who had killed him, and why.
+
+3
+
+My name is Christopher John Francis Boone. I know all the countries of the world and their capital cities and every prime number up to 7,057.
+
+Eight years ago, when I first met Siobhan, she showed me this picture
+
+[sad face]
+
+and I knew that it meant ‘sad,’ which is what I felt when I found the dead dog.
+
+Then she showed me this picture
+
+[smiley face]
+
+and I knew that it meant ‘happy’, like when I’m reading about the Apollo space missions, or when I am still awake at 3 am or 4 am in the morning and I can walk up and down the street and pretend that I am the only person in the whole world.
+
+Then she drew some other pictures
+
+[various happy, sad, confused, surprised faces]
+
+but I was unable to say what these meant.
+
+I got Siobhan to draw lots of these faces and then write down next to them exactly what they meant. I kept the piece the piece of paper in my pocket and took it out when I didn’t understand what someone was saying. But it was very difficult to decide which of the diagrams was most like the face they were making because people’s faces move very quickly.
+
+When I told Siobhan that I was doing this, she got out a pencil and another piece of paper and said it probably made people feel very
+
+[confused face]
+
+and then she laughed. So I tore the original piece of paper up and threw it away. And Siobhan apologised. And now if I don’t know what someone is saying I ask them what they mean or I walk away.
+
+5
+
+I pulled the fork out of the dog and lifted him into my arms and hugged him. He was leaking blood from the fork-holes.
+
+I like dogs. You always know what a dog is thinking. It has four moods. Happy, sad, cross and concentrating. Also, dogs are faithful and they do not tell lies because they cannot talk.
+
+I had been hugging the dog for 4 minutes when I heard screaming. I looked up and saw Mrs Shears running towards me from the patio. She was wearing pyjamas and a housecoat. Her toenails were painted bright pink and she had no shoes on.
+
+She was shouting, ‘What in fuck’s name have you done to my dog?’.
+
+I do not like people shouting at me. It makes me scared that they are going to hit me or touch me and I do not know what is going to happen.
+
+‘Let go of the dog,’ she shouted. ‘Let go of the fucking dog for Christ’s sake.’
+
+I put the dog down on the lawn and moved back 2 metres.
+
+She bent down. I thought she was going to pick the dog up herself, but she didn’t. Perhaps she noticed how much blood there was and didn’t want to get dirty. Instead, she started screaming again.
+
+I put my hands over my ears and closed my eyes and rolled forward till I was hunched up with my forehead pressed onto the grass. The grass was wet and cold. It was nice.
+
+7
+
+This is a murder mystery novel.
+
+Siobhan said that I should write something I would want to read myself. Mostly I read books about science and maths. I do not like proper novels. In proper novels people say things like, ‘I am veined with iron, with silver and with streaks of common mud. I cannot contract into the firm fist which those clench who do not depend on stimulus’ . What does this mean? I do not know. Nor does Father. Nor do Siobhan or Mr Jeavons. I have asked them.
+
+Siobhan has long blonde hair and wears glasses which are made of green plastic. And Mr Jeavons smells of soap and wears brown shoes that have approximately 60 tiny circular holes in each of them.
+
+But I do like murder mystery novels. So I am writing a murder mystery novel.
+
+In a murder mystery novel someone has to work out who the murderer is and then catch them. It is a puzzle. If it is a good puzzle you can sometimes work out the answer before the end of the book.
+
+Siobhan said that the book should begin with something to grab people’s attention. That is why I started with the dog. I also started with the dog because it happened to me and I find it hard to imagine things which did not happen to me.
+
+Siobhan read the first page and said that it was different. She put this word into inverted commas by making the wiggly quotation sign with her first and second fingers. She said that it was usually people who were killed in murder mystery novels. I said that two dogs were killed in The Hound of the Baskervilles, the hound itself and James Mortimer’s spaniel, but Siobhan said they weren’t the victims of the murder, Sir Charles Baskerville was. She said that this was because readers cared more about people than dogs, so if a person was killed in the book readers would want to carry on reading.
+
+I said that I wanted to write about something real and I knew people who had died but I did not know any people who had been killed, except Edward’s father from school, Mr Paulson, and that was a gliding accident, not murder, and I didn’t really know him. I also said that I cared about dogs because they were faithful and honest, and some dogs were cleverer and more interesting than some people. Steve, for example, who comes to centre on Thursdays, needs help to eat his food and could not even fetch a stick. Siobhan asked me not to say this to Steve’s mother.
+
+11
+
+Then the police arrived. I like the police. They have uniforms and numbers and you know what they are meant to be doing. There was a policewoman and a policeman. The policewoman had a little hole in her tights on her left ankle and a red scratch in the middle of the hole. The policeman had a big orange leaf stuck to the bottom of his shoe which was poking out from one side.
+
+The policewoman put her arms round Mrs Shears and led her back towards the house.
+
+I lifted my head off the grass.
+
+The policeman squatted down beside me and said, ‘Would you like to tell me what’s going on here, young man?’.
+
+I sat up and said ‘The dog is dead.’
+
+‘I’d got that far,’ he said.
+
+I said, ‘I think someone killed the dog.’
+
+‘How old are you?’ he asked.
+
+I replied, ‘I am 15 years and 3 months and 2 days.’
+
+‘And what, precisely, were you doing in the garden?’ he asked.
+
+‘I was holding the dog,’ I replied.
+
+‘And why were you holding the dog?’ he asked.
+
+This was a difficult question. It was something I wanted to do. I like dogs. It made me sad to see that the dog was dead.
